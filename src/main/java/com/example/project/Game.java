33815dcef50a696ae64793c23f49513a8ee0ea48
daniels011 <dsergeyev22@gmail.com>
2025-03-07 04:06:07 +0000

    Commit 2

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -1,8 +1,6 @@
 package com.example.project;
 import java.util.ArrayList;
 
-
-
 public class Game{
     public static String determineWinner(Player p1, Player p2, String p1Hand, String p2Hand, ArrayList<Card> communityCards) {
         //generates and stores the hand rankings of each hand
